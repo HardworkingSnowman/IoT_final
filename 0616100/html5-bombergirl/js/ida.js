af7f8b803837896bc09af5029cb6fda9bf964842
MapleTang <QQqiyuan98@hotmail.com>
2019-06-10 18:28:33 +0800

    Added Smartphone odf v1

---
+++ 0616100/html5-bombergirl/js/ida.js
@@ -0,0 +1,45 @@
+ $(function(){
+        csmapi.set_endpoint ('http://140.113.199.189:9999');
+        var profile = {
+		    'dm_name': 'Smartphone_0616100',          
+			'idf_list':[],
+			'odf_list':[Acceleration_0616100],
+		        'd_name': 'Smartphone0616100',
+        };
+        function Acceleration_0616100(data){
+            if(Math.abs(data[0]) > Math.abs(data[1])){
+                gInputEngine.actions['sniper_down'] = false;
+                gInputEngine.actions['sniper_up'] = false;
+                if(data[0]>0){
+                    gInputEngine.actions['sniper_right'] = false;
+                    gInputEngine.actions['sniper_left'] = true;
+                }
+                else{
+                    gInputEngine.actions['sniper_right'] = true;
+                    gInputEngine.actions['sniper_left'] = false;
+                }
+            }
+            else{
+                gInputEngine.actions['sniper_right'] = false;
+                gInputEngine.actions['sniper_left'] = false;
+                if(data[1]>0){
+                    gInputEngine.actions['sniper_down'] = false;
+                    gInputEngine.actions['sniper_up'] = true;
+                }
+                else{
+                    gInputEngine.actions['sniper_down'] = true;
+                    gInputEngine.actions['sniper_up'] = false;
+                }
+            }
+        }
+      
+/*******************************************************************/                
+
+        function ida_init(){
+	    console.log(profile.d_name);
+	}
+        var ida = {
+            'ida_init': ida_init,
+        }; 
+        dai(profile,ida);     
+});
